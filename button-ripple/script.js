435314336038631ba39d91d91d3379c4fc61fb2c
memset0 <34177126+memset0@users.noreply.github.com>
2021-09-25 10:37:50 +0800

    button-ripple demo

---
+++ button-ripple/script.js
@@ -0,0 +1,16 @@
+Array.from(document.getElementsByClassName('btn-ripple')).forEach($btn => {
+  $btn.onclick = event => {
+    const $ripple = document.createElement('div');
+    const x = event.x - $btn.getBoundingClientRect().x - 10;
+    const y = event.y - $btn.getBoundingClientRect().y - 10;
+    $ripple.style.left = x + 'px';
+    $ripple.style.top = y + 'px';
+    $ripple.classList.add('ripple');
+    $btn.appendChild($ripple);
+    const listener = function() {
+      $btn.removeChild($ripple);
+      $ripple.removeEventListener('animationend', listener);
+    };
+    $ripple.addEventListener('animationend', listener);
+  };
+});
